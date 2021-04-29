1eaca19d6f4b2952f34aa0f4c161d90b960df1ed
markusgerman <german@myconsult.de>
2021-04-29 12:56:15 +0200

    Querybuilder

---
+++ src/Utils/Database/Nutzer.java
@@ -0,0 +1,17 @@
+package Utils.Database;
+
+import java.util.Date;
+
+public class Nutzer {
+    public String nutzer_nr;
+    public String adresse;
+    public String mail;
+    public Date geburtstag;
+    public String nachname;
+    public String nutzername;
+    public String passwort;
+    public Object profilbild;
+    public int rollen_nr;
+    public int tarif_nr;
+    public String vorname;
+}
---
+++ src/Utils/Database/Tarif.java
@@ -0,0 +1,7 @@
+package Utils.Database;
+
+public class Tarif {
+    public int tarifnummer;
+    public String bezeichnung;
+    public double preis;
+}
