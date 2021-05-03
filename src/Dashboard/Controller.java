1257e865566995303bbb1e37b1acea1eebf74e05
MarvinVoll <marvin.v@gmx.net>
2021-05-03 12:45:07 +0200

    ABR-08: added countAllUser method

--- src/Dashboard/Controller.java
+++ src/Dashboard/Controller.java
@@ -1,22 +1,30 @@
 package Dashboard;
 
 import Utils.Database.Nutzer;
-
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.Console;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class Controller {
-    public TextArea customers;
-
-    public Controller() throws SQLException {
-        System.out.println("test");
-        Nutzer n = new Nutzer();
-        n.countAllUser();
-    }
+public class  Controller implements Initializable {
+    public Text customers;
 
     public void DashboardController(ActionEvent actionEvent) {
 
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Nutzer n = new Nutzer();
+        try {
+            customers.setText(Integer.toString(n.countAllUser()));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        //customers.insert(Integer.toString(n.countAllUser()),1);
     }
 }
