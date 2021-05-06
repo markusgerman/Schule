c39f549f3b489016035a1aa0abe4399e43a066fd
MarvinVoll <marvin.v@gmx.net>
2021-05-06 21:45:09 +0200

    made some ui stuff

--- src/Router/DocumentRouterController.java
+++ src/Router/DocumentRouterController.java
@@ -2,15 +2,20 @@ package Router;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 /**
  * Steuerung für die Umschaltung der einzelnen Fenster in der Side-bar.
  */
-public class DocumentRouterController {
+public class DocumentRouterController implements Initializable {
 
     @FXML
     public BorderPane mainPane;
@@ -42,6 +47,13 @@ public class DocumentRouterController {
     public void handleFakturaButton(ActionEvent actionEvent) {
         FxmlLoader loader = new FxmlLoader();
         Pane pane = loader.getPage("Faktura/UI_Faktura");
+        mainPane.setCenter(pane);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        FxmlLoader loader = new FxmlLoader();
+        Pane pane = loader.getPage("Dashboard/UI_Dashboard");
         mainPane.setCenter(pane);
     }
 }
--- src/UserAuth/Controller.java
+++ src/UserAuth/Controller.java
@@ -4,10 +4,14 @@ package UserAuth;
 import Utils.Database.Nutzer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import sample.Main;
 import javax.xml.soap.Text;
 import java.io.IOException;
@@ -28,8 +32,13 @@ public class Controller {
 
         try {
             if(Main.debug){
-                VBox pane = FXMLLoader.load(getClass().getResource("../sample/sample.fxml"));
-                vBox.getChildren().setAll(pane);
+                Parent root = FXMLLoader.load(getClass().getResource("../sample/sample.fxml"));
+
+                Stage primaryStage = new Stage();
+                primaryStage.setTitle("Abrechnungs- und Tarifverwaltungmodul");
+                primaryStage.setScene(new Scene(root));
+                primaryStage.show();
+                ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
             }
             if (loginNutzer.readAuthentication() == true){
                 VBox pane = FXMLLoader.load(getClass().getResource("../sample/sample.fxml"));
